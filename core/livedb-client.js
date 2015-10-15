7145a9b4a05a7b244d4073aee33ceb1aaa2713ff
Alexander Treptau <atreptau@welfenlab.de>
2015-10-15 13:46:25 +0200

    (release/fix) 0.6.3 update ws dependencie and add option to use livedb inmemory store

--- core/livedb-client.js
+++ core/livedb-client.js
@@ -1,6 +1,14 @@
 module.exports = function (config)
 {
   var Promise = require('bluebird');
+
+  if (!config.sharejs.rethinkdb) {
+	console.log("Warning using the livedb inmemory store and NOT RethinkDB!");
+	var livedb = require('livedb');
+	var db = livedb.memory();
+	var backend = Promise.promisifyAll(livedb.client(db));
+  	return backend;
+  }
   // Connect to our RethinkDB database
   var db = require('livedb-rethinkdb')(config.sharejs.rethinkdb);
   var livedb = require('livedb');
