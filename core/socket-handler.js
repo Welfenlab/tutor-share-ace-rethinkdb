42d7b217aec8d9a114f6e9aaac66e65e90bc4dc8
Alexander Treptau <atreptau@welfenlab.de>
2015-12-11 16:38:50 +0100

    (feature) Do not allow operations after the due date and use spine.log for logging spine.log prints only if developing

--- core/socket-handler.js
+++ core/socket-handler.js
@@ -1,71 +1,70 @@
-module.exports = function (spine)
-{
-  var sharejs = require('share');
-  var Duplex = require('stream').Duplex;
-  var backend = require('./livedb-client')(spine.config);
+module.exports = function (spine) {
+  var sharejs = require('share')
+  var Duplex = require('stream').Duplex
+  var backend = require('./livedb-client')(spine.config)
   var share = sharejs.server.createClient({
     backend: backend
-  });
+  })
 
   // Handle socket connections
   var socketConnectionHandler = function (client) {
     var stream = new Duplex({
       objectMode: true
-    });
+    })
 
     // Send new message to all clients
     stream._write = function (chunk, encoding, callback) {
-      console.log('Stream : write | UID', client.upgradeReq.session.uid, "sessionID", client.upgradeReq.sessionID, "data" , JSON.stringify(chunk));
-      client.send(JSON.stringify(chunk));
-      return callback();
-    };
+      spine.log('Stream : write | UID' + client.upgradeReq.session.uid + 'sessionID' + client.upgradeReq.sessionID + 'data' + JSON.stringify(chunk))
+      client.send(JSON.stringify(chunk))
+      return callback()
+    }
 
-    stream._read = function () {};
+    stream._read = function () {}
 
-    var request = client.upgradeReq;
-    stream.headers = request.headers;
-    stream.remoteAddress = request.connection.remoteAddress;
+    var request = client.upgradeReq
+    stream.headers = request.headers
+    stream.remoteAddress = request.connection.remoteAddress
 
-    var remoteEndpoint = stream.remoteAddress + ':' + client._socket.remotePort;
+    var remoteEndpoint = stream.remoteAddress + ':' + client._socket.remotePort
 
     stream.on('error', function (msg) {
-      spine.log('an error occured for ' + remoteEndpoint);
-      return client.close(msg);
-    });
+      spine.log('an error occured for ' + remoteEndpoint)
+      return client.close(msg)
+    })
 
     // When server receives message from the client
     client.on('message', function (data) {
       try {
-        var dat = JSON.parse(data);
+        var dat = JSON.parse(data)
       } catch (e) {
-        //eg failed to parse the data
-        console.log("ws message parse error: ", e);
-        this.emit('end');
+        // eg failed to parse the data
+        spine.log('ws message parse error: ' + e)
+        this.emit('end')
       }
 
-      spine.operationAllowed(dat, request).then( function(check){
-        console.log('recieved message');
-      }).then(function(){
-        return stream.push(dat);
-      }).catch(function(err){
-        spine.log('unpermitted operation by ' + remoteEndpoint + ' with reason ' + err);
-        return client.close();
-      });
-    });
+      spine.operationAllowed(dat, request).then(function (check) {
+        spine.log('recieved message')
+      }).then(function () {
+        return stream.push(dat)
+      }).catch(function (err) {
+        spine.log('unpermitted operation by ' + remoteEndpoint + ' with reason ' + err)
+        return client.close()
+      })
+    })
 
     client.on('close', function (reason) {
-      spine.log('closed connection to ' + remoteEndpoint + ' with reason ' + reason);
-      stream.push(null);
-      stream.emit('close');
-      return client.close(reason);
-    });
+      spine.log('closed connection to ' + remoteEndpoint + ' with reason ' + reason)
+      stream.push(null)
+      stream.emit('close')
+      return client.close(reason)
+    })
 
     stream.on('end', function () {
-      return client.close();
-    });
+      return client.close()
+    })
 
-    return share.listen(stream);
-  };
+    return share.listen(stream)
+  }
 
-  return socketConnectionHandler;
+  return socketConnectionHandler
 }
--- share-ace-server.js
+++ share-ace-server.js
@@ -3,69 +3,61 @@
  *  @param config An object containing a config
  */
 
-var moment = require('moment');
+var moment = require('moment')
 
-function toUUID(input) {
+function toUUID (input) {
   // ee25 6059 - 9d92 - 4774 - 9db2 - 4563 78e0 4586
-  return input.slice(0,8)   + "-" +
-         input.slice(8,12)  + "-" +
-         input.slice(12,16) + "-" +
-         input.slice(16,20) + "-" +
-         input.slice(20,32);
+  return input.slice(0, 8) + '-' +
+  input.slice(8, 12) + '-' +
+  input.slice(12, 16) + '-' +
+  input.slice(16, 20) + '-' +
+  input.slice(20, 32)
 }
 
 module.exports = function (DB) {
-
-return function(app, config)
-  {
-    var dconv = require("./src/doc_name_converter")(config);
+  return function (app, config) {
+    var dconv = require('./src/doc_name_converter')(config)
     var serverSpine = {
       'config': config,
-      'operationAllowed': function(data, request) {
-        return new Promise(function(fulfill, reject){
-          var docData = dconv.doc2Group(data);
-          dconv.session2Group(request.session,DB).then( function(groupData) {
-            if (groupData.id != docData.group){
-              reject("Group '"+groupData.id+"' tried to access document " + JSON.stringify(docData));
-              return;
+      'operationAllowed': function (data, request) {
+        return new Promise(function (fulfill, reject) {
+          var docData = dconv.doc2Group(data)
+          dconv.session2Group(request.session, DB).then(function (groupData) {
+            if (groupData.id != docData.group) {
+              reject("Group '" + groupData.id + "' tried to access document " + JSON.stringify(docData))
+              return
             }
-            fulfill();
+            fulfill()
           })
-          /*
-          .then( function(){
-            var task;
-            if (data.s) task = Object.getOwnPropertyNames(data.s[group])[0];
-            else        task = data.d;
-            task = toUUID(task);
+            .then(function () {
+              return docData.exercise
+            })
+            .then(DB.Exercises.getById)
+            .then(function (exercise) {
+              // NOTE: This should work, needs testing
+              var dueDateReached = moment(exercise.dueDate).isBefore()
+              if (dueDateReached && data.a != 'bs')
+                reject('due date reached')
+            })
 
-            return {id: task};
-          })
-          .then(DB.Exercises.getById)
-          .then( function(exercise) {
-            var dueDateReached = moment(exercise.dueDate).isBefore();
-
-            if (dueDateReached && data.a != 'bs')
-              reject("due date reached");
-          })*/
-          .catch(function(error){
-            console.log("operationAllowed: ", error);
-            reject("error");
-          });
-        });
+            .catch(function (error) {
+              console.log('operationAllowed: ', error)
+              reject('error')
+            })
+        })
       },
       'log': function (obj) {
         if (config.development)
-          console.log('(share-ace) ' + obj);
-        },
-      'path': "/api/ws"
-    };
+          console.log('(share-ace) ' + obj)
+      },
+      'path': '/api/ws'
+    }
 
+    var shareJSServer = require('./core/sharejs-server')(serverSpine, app)
+    // var socketHandler = require('./core/socket-handler')(serverSpine)
 
-    var shareJSServer = require('./core/sharejs-server')(serverSpine, app);
-    //var socketHandler = require('./core/socket-handler')(serverSpine);
-
-    //shareJSServer.listen(config.sharejs.port);
+    // shareJSServer.listen(config.sharejs.port)
     console.log('sharejs server listens now on ' + serverSpine.path)
-}
+  }
 
 }
